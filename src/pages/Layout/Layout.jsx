fbd4142ddc68829045663938365fc9382004b5c5
EvaMaria289 <131320729+EvaMaria289@users.noreply.github.com>
2023-08-10 17:43:40 -0400

    Update Layout.jsx

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../../components/Footer/Footer";
+
 import Header from "../../components/Header/Header";
 import { useContext } from "react";
 import { BackgroundContext } from "../../context/BackgroundContext";
@@ -16,7 +16,7 @@ return(
         <Outlet/>
     </body>
     
-    <Footer/>
+   
 </>)
 }
 
